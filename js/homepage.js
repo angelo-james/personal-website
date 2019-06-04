28ea8aa6ca7a2920f16585e057c5378e4fb64380
AJ Arriola <angelojames.arriola@gmail.com>
2019-06-04 14:08:50 -0700

    initial commit for development branch

--- js/homepage.js
+++ js/homepage.js
@@ -94,3 +94,21 @@ function check_if_in_view() {
 
 $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modalImg = document.getElementById("img01");
+function resumeModal(resume) {
+  modal.style.display = "block";
+  modalImg.src = resume;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
