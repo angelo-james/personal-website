6f7cd4644061828acac684aa93b351ce79d5d654
AJ <angelojames.arriola@gmail.com>
2021-12-03 21:44:46 -0800

    Add theme provider to app component. create navbar and add to container block

--- src/components/ContainerBlock/ContainerBlock.js
+++ src/components/ContainerBlock/ContainerBlock.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Navbar from "../Navbar/Navbar";
 
 export default function ContainerBlock({ children, ...meta }) {
   return (
@@ -8,7 +9,8 @@ export default function ContainerBlock({ children, ...meta }) {
         <title>{meta.title}</title>
       </Head>
       <main>
-        <div>{children}</div>
+        <Navbar />
+        <div className="max-w-6xl  mx-auto px-4 py-10 md:py-20">{children}</div>
       </main>
     </div>
   );
