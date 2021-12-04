ff3997c78b1f2dc5fec777b5efab61bd0aa0ee11
AJ <angelojames.arriola@gmail.com>
2021-12-03 21:36:46 -0800

    create file for my data

---
+++ src/constants/data.js
@@ -0,0 +1,39 @@
+const userData = {
+  githubUsername: "angelo-james",
+  name: "AJ Arriola",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "angelojames.arriola@gmail.com",
+  phone: "+1 650 452 4055",
+  address: "San Francisco Bay Area",
+  projects: [
+    {
+      title: "",
+      link: "",
+      imgUrl: "",
+    },
+  ],
+  about: {
+    title: "",
+    description: [""],
+    currentProject: "",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Jr. Software Developer",
+      company: "Q CELLS",
+      year: "2021",
+      companyLink: "",
+      desc: "",
+    },
+  ],
+  resumeUrl: "",
+  socialLinks: {
+    instagram: "https://www.instagram.com/angelo.james/",
+    linkedin: "https://www.linkedin.com/in/ajarriola/",
+    github: "https://github.com/angelo-james",
+  },
+};
+
+export default userData;
