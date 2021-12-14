6e67911f3f02849793bada2c0f87721b138d5326
angelo-james <angelojames.arriola@gmail.com>
2021-12-13 21:39:28 -0800

    add content to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import ContainerBlock from "../components/ContainerBlock/ContainerBlock";
 export default function Home() {
   return (
     <ContainerBlock title="AJ Arriola - Developer by Design">
-      <h1>Home page</h1>
+      <h1 className="text-7xl">Welcome To <br/> My Personal Portfolio</h1>
+      <p className="font-light text-base mt-10">The purpose of this website is to share a little bit about myself and whats going on during my constant growth as a Full-Stack Developer</p>
     </ContainerBlock>
   );
 }
